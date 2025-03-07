b29acffa80469eff26dbe038a681a2debab74bd9
Mirko <mirkocaccavone03@gmail.com>
2025-03-07 18:43:08 +0100

    [feat] add middleware for 404 handling and error management, serve static files, and set up main route

--- app.js
+++ app.js
@@ -4,6 +4,10 @@ const app = express()
 const port = 3000
 const cors = require("cors")
 
+// importo i middleware
+const notFound = require('./middleware/notFound');
+const errorsHandler = require('./middleware/errorsHandler');
+
 // middleware per il CORS
 app.use(cors({
     origin: '*'
@@ -15,6 +19,24 @@ const postsRouter = require('./routers/routes');
 // Usa il router per le richieste alla route '/posts'
 app.use("/posts", postsRouter)
 
+// Serve i file statici dalla cartella 'public'
+app.use(express.static('public'));
+
+// registro il body-parser per "application/json"
+// interpreta quello che sarà passato come file JSON
+app.use(express.json());
+
+// Gestisce la route principale ('/')
+app.get('/', (req, res) => {
+    // Invia una semplice risposta testuale
+    res.send("Server del mio blog")
+});
+
+// utilizzo middleware di gestione not found 404
+app.use(notFound);
+
+// utilizzo middleware di gestione errore server
+app.use(errorsHandler);
 
 // Avvia il server sulla porta specificata
 app.listen(port, () => {
