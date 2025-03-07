f7b4925a3a39f616fc960af71473da712297e6a2
Mirko <mirkocaccavone03@gmail.com>
2025-03-07 19:35:37 +0100

    [feat] add destroy function to delete posts from the database

--- controllers/controller_routes.js
+++ controllers/controller_routes.js
@@ -8,12 +8,28 @@ function index(req, res) {
 
     // eseguiamo la query
     connection.query(sql, (err, results) => {
-
-        if (err) return res.status(500).json({ error: 'Database query failed' });
+        if (err)
+            return res.status(500).json({ error: 'Database query failed' });
 
         res.json(results);
-
     });
 }
 
-module.exports = { index }
+// funzione destroy
+function destroy(req, res) {
+
+    // recuperiamo l'id dall' URL e trasformiamolo in numero
+    const id = parseInt(req.params.id)
+
+    // prepariamo la query
+    const sql = 'DELETE FROM posts WHERE id = ?';
+
+    // eseguiamo la query ed eliminiamo il post
+    connection.query(sql, [id], (err) => {
+        if (err) return res.status(500).json({ error: 'Failed to delete post' });
+        res.sendStatus(204)
+    });
+
+}
+
+module.exports = { index, destroy }
