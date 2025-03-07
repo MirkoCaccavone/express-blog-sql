fef26bea1cf03ce7436ce280c91284f7514bb418
Mirko <mirkocaccavone03@gmail.com>
2025-03-07 17:55:52 +0100

    [feat] enable CORS and set up posts router

--- app.js
+++ app.js
@@ -3,3 +3,20 @@ const express = require('express')
 const app = express()
 const port = 3000
 const cors = require("cors")
+
+// middleware per il CORS
+app.use(cors({
+    origin: '*'
+}));
+
+// importo il router
+const postsRouter = require('./routers/routes');
+
+// Usa il router per le richieste alla route '/posts'
+app.use("/posts", postsRouter)
+
+
+// Avvia il server sulla porta specificata
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
